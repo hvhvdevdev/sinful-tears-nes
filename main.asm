39d5293298656487102933af26622cd3e1ade224
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-10-23 21:44:05 +0700

    ...

--- main.asm
+++ main.asm
@@ -7,19 +7,35 @@
 ;; INES header setup.
 ;;
 ;;===================================================================
-    .inesprg 1              ; Bank of program code.
-    .ineschr 1              ; Bank of picture data.
-    .inesmap 0              ; Mapper.
-    .inesmir 1              ; Mirror setting.
+    .INESPRG  1              ; Bank of program code.
+    .INESCHR  1              ; Bank of picture data.
+    .INESMAP  0              ; Mapper.
+    .INESMIR  3              ; Mirror setting.
 
 ;;===================================================================
 ;;
-;; Bank 0.
+;; Bank 1.
 ;;
 ;;===================================================================
-    .bank 0
+    .BANK 1
+    .ORG  $FFFA
     
-    .org $8000
+    .WORD  0
+    .WORD  RESET
+    .WORD  0
+
+;;===================================================================
+;;
+;; Bank 0: Program.
+;;
+;;===================================================================
+    .BANK 0
+    .ORG  $0000
+    
+PCC_XPOS:  .DB  0
+PCC_YPOS:  .DB  0
+    
+    .ORG  $8000
 
 ;;===================================================================
 ;;
@@ -37,12 +53,89 @@ RESET:
     STX  $2000    
     STX  $2001    
     STX  $4010
-    JMP  FOREVER
-
+    
+  ; Setup PPU.
+    LDA  #%00001000
+    STA  $2000
+    LDA  #%00011110
+    STA  $2001
+    LDX  #$00
+    LDA  #$3F
+    STA  $2006
+    LDA  #$00
+    STA  $2006
+    
+  .LOAD_PALLETE:
+    LDA  TILE_PALLETE, X
+    STA  $2007
+    INX  
+    CPX  #32
+    BNE  .LOAD_PALLETE
+  
+  ; Setup positions.  
+    LDA  #20
+    STA  PCC_XPOS
+    STA  PCC_YPOS
+    
+  ; Start looping forever.  
+    JMP FOREVER
+    
 ;;===================================================================
 ;;
 ;; Loop forever.
 ;;
 ;;===================================================================
 FOREVER:
-    JMP FOREVER
+    LDX  <PCC_YPOS
+    INX
+    STX  <PCC_YPOS
+    LDX  <PCC_XPOS
+    INX
+    INX 
+    STX  <PCC_XPOS
+    JSR  WAIT_VBLANK 
+    JMP  FOREVER
+
+;;===================================================================
+;;
+;; Wait for Vblank.
+;;
+;;===================================================================
+WAIT_VBLANK:
+    LDA  $2002
+    BPL  WAIT_VBLANK
+  ; Tell $2003 to tell $2004 to start at $0000
+    LDA  #$00
+    STA  $2003
+    LDA  #$00
+    STA  $2003
+  ; Load and store PCC_YPOS
+    LDA  <PCC_YPOS
+    STA  $2004
+    LDA  #$00
+    STA  $2004
+    LDA  #$00
+    STA  $2004
+    LDA  <PCC_XPOS
+    STA  $2004
+    RTS
+
+
+;;===================================================================
+;;
+;; TILE PALLETE
+;;
+;;===================================================================
+TILE_PALLETE:
+    .INCBIN "our.pal"
+    
+;;===================================================================
+;;
+;; Bank 2.
+;;
+;;===================================================================
+    .BANK 2
+    .ORG  $0000
+    
+    .INCBIN "our.bkg"
+    .INCBIN "our.spr"
