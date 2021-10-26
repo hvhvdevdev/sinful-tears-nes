df5008381306f1a65c9e83c9b68286edf963b873
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-10-26 17:15:57 +0700

    code style

--- main.asm
+++ main.asm
@@ -10,35 +10,35 @@
 ;; INES header setup.
 ;;
 ;;==============================================================================
-    .INESPRG  1              ; Bank of program code.
-    .INESCHR  1              ; Bank of picture data.
-    .INESMAP  0              ; Mapper.
-    .INESMIR  0              ; Mirror setting.
+    .INESPRG    1              ; Bank of program code.
+    .INESCHR    1              ; Bank of picture data.
+    .INESMAP    0              ; Mapper.
+    .INESMIR    0              ; Mirror setting.
 
 ;;==============================================================================
 ;;
 ;; Bank 1.
 ;;
 ;;==============================================================================
-    .BANK 1
-    .ORG  $FFFA
+    .BANK   1
+    .ORG    $FFFA
     
-    .WORD  0
-    .WORD  RESET
-    .WORD  0
+    .WORD   0
+    .WORD   RESET
+    .WORD   0
 
 ;;==============================================================================
 ;;
 ;; Bank 0: Program.
 ;;
 ;;==============================================================================
-    .BANK 0
-    .ORG  $0000
+    .BANK   0
+    .ORG    $0000
     
-PCC_XPOS:  .RS  1
-PCC_YPOS:  .RS  1
+PCC_XPOS:   .RS     1
+PCC_YPOS:   .RS     1
     
-    .ORG  $8000
+    .ORG    $8000
 
 ;;==============================================================================
 ;;
@@ -48,41 +48,41 @@ PCC_YPOS:  .RS  1
 RESET:
     SEI
     CLD
-    LDX  #$40    
-    STX  $4017    
-    LDX  #$FF    
+    LDX     #$40    
+    STX     $4017    
+    LDX     #$FF    
     TXS          
     INX
-    STX  $2000    
-    STX  $2001    
-    STX  $4010
+    STX     $2000    
+    STX     $2001    
+    STX     $4010
     
   ; Setup PPU.
-    LDA  #%00001000
-    STA  $2000
-    LDA  #%00011110
-    STA  $2001
-    LDX  #$00
-    LDA  #$3F
-    STA  $2006
-    LDA  #$00
-    STA  $2006
-    LDX  #$00
+    LDA     #%00001000
+    STA     $2000
+    LDA     #%00011110
+    STA     $2001
+    LDX     #$00
+    LDA     #$3F
+    STA     $2006
+    LDA     #$00
+    STA     $2006
+    LDX     #$00
     
   .LOAD_PALLETE:
-    LDA  TILE_PALLETE, X
-    STA  $2007
+    LDA     TILE_PALLETE, X
+    STA     $2007
     INX  
-    CPX  #32
-    BNE  .LOAD_PALLETE
+    CPX     #32
+    BNE     .LOAD_PALLETE
   
   ; Setup positions.  
-    LDA  #20
-    STA  PCC_XPOS
-    STA  PCC_YPOS
+    LDA     #20
+    STA     PCC_XPOS
+    STA     PCC_YPOS
     
   ; Start looping forever.  
-    JMP FOREVER
+    JMP     FOREVER
     
 ;;==============================================================================
 ;;
@@ -90,15 +90,15 @@ RESET:
 ;;
 ;;==============================================================================
 FOREVER:
-    LDX  <PCC_YPOS
+    LDX     <PCC_YPOS
     INX
-    STX  <PCC_YPOS
-    LDX  <PCC_XPOS
+    STX     <PCC_YPOS
+    LDX     <PCC_XPOS
     INX
     INX 
-    STX  <PCC_XPOS
-    JSR  WAIT_VBLANK 
-    JMP  FOREVER
+    STX     <PCC_XPOS
+    JSR     WAIT_VBLANK 
+    JMP     FOREVER
 
 ;;==============================================================================
 ;;
@@ -106,20 +106,20 @@ FOREVER:
 ;;
 ;;==============================================================================
 WAIT_VBLANK:
-    LDA  $2002
-    BPL  WAIT_VBLANK
+    LDA     $2002
+    BPL     WAIT_VBLANK
   ; Tell $2003 to tell $2004 to start at $0000
-    LDA  #$00
-    STA  $2003
+    LDA     #$00
+    STA     $2003
   ; Load and store PCC_YPOS
-    LDA  <PCC_YPOS
-    STA  $2004
-    LDA  #$00
-    STA  $2004
-    LDA  #$00
-    STA  $2004
-    LDA  <PCC_XPOS
-    STA  $2004
+    LDA     <PCC_YPOS
+    STA     $2004
+    LDA     #$00
+    STA     $2004
+    LDA     #$00
+    STA     $2004
+    LDA     <PCC_XPOS
+    STA     $2004
     RTS
 
 
@@ -130,16 +130,16 @@ WAIT_VBLANK:
 ;;==============================================================================
 TILE_PALLETE:
 ;   Backgrounds.    
-    .BYTE  $0F                   ; Universal Background.
-    .BYTE  $1b, $1f, $03, $0F    ; Background Palette 0
-    .BYTE  $1c, $1b, $07, $0F    ; Background Palette 1
-    .BYTE  $1d, $1b, $07, $0F    ; Background Palette 2
-    .BYTE  $1b, $1b, $07, $3D    ; Background Palette 3
+    .BYTE   $0F                   ; Universal Background.
+    .BYTE   $1b, $1f, $03, $0F    ; Background Palette 0
+    .BYTE   $1c, $1b, $07, $0F    ; Background Palette 1
+    .BYTE   $1d, $1b, $07, $0F    ; Background Palette 2
+    .BYTE   $1b, $1b, $07, $3D    ; Background Palette 3
 ;   Sprites.
-    .BYTE  $1c, $03, $09, $0F    ; Sprite Palette 0
-    .BYTE  $1b, $1b, $07, $0F    ; Sprite Palette 1
-    .BYTE  $1b, $1b, $07, $0F    ; Sprite Palette 2
-    .BYTE  $1b, $1b, $06         ; Sprite Palette 3
+    .BYTE   $1c, $03, $09, $0F    ; Sprite Palette 0
+    .BYTE   $1b, $1b, $07, $0F    ; Sprite Palette 1
+    .BYTE   $1b, $1b, $07, $0F    ; Sprite Palette 2
+    .BYTE   $1b, $1b, $06         ; Sprite Palette 3
 
     
 ;;==============================================================================
@@ -147,18 +147,18 @@ TILE_PALLETE:
 ;; Bank 2.
 ;;
 ;;==============================================================================
-    .BANK  2
-    .ORG   $0000
+    .BANK   2
+    .ORG    $0000
 
 ;   Backgrounds.    
-    .DS    4096
-    .BYTE  %10000001, %00000001, %00000001, %00000001
-    .BYTE  %00000001, %00000000, %00000000, %00000001
+    .DS     4096
+    .BYTE   %10000001, %00000001, %00000001, %00000001
+    .BYTE   %00000001, %00000000, %00000000, %00000001
     
 ;   Sprites.
-    .ORG   $1000
+    .ORG    $1000
 ;   Sprite 0
-    .BYTE  %10000001, %00000001, %00000001, %00000001
-    .BYTE  %00000001, %00000000, %00000000, %00000001
-    .BYTE  %10000000, %00000000, %00000000, %00000001
-    .BYTE  %00000000, %11111111, %00000000, %00000001
+    .BYTE   %10000001, %00000001, %00000001, %00000001
+    .BYTE   %00000001, %00000000, %00000000, %00000001
+    .BYTE   %10000000, %00000000, %00000000, %00000001
+    .BYTE   %00000000, %11111111, %00000000, %00000001
