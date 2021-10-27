768850245f3bcf3ab29b8c1082674651a96a7abf
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-10-27 14:45:14 +0700

    So much changes...

--- main.asm
+++ main.asm
@@ -37,10 +37,23 @@
 ;                                                                              ;
 ;******************************************************************************;
     .BANK   0
-    .ORG    $0000
-;    
-PCC_XPOS:   .RS     1
-PCC_YPOS:   .RS     1
+    .RSSET    $0000
+;
+;******************************************************************************;
+;                                                                              ;
+;   Variables & Constants                                                      ;
+;                                                                              ;
+;******************************************************************************;
+FRAME_DELAY_AMOUNT  EQU     2         
+FRAME_MOD_255:      .RS     1
+FRAME_DELAY:        .RS     1
+SPRITE_FRAME        .RS     1
+SPRITE_FRAME_DELAY  .RS     1
+;
+PCC_XPOS:           .RS     1
+PCC_YPOS:           .RS     1
+;
+CONTROLLER          .RS     1
 ;    
     .ORG    $8000
 ;
@@ -57,13 +70,11 @@ RESET:
     STX     $4017
 ;   Stack initialization.    
     LDX     #$FF    
-    TXS          
-    INX
-    STX     $2000    
-    STX     $2001    
-    STX     $4010    
+    TXS           
 ;   Setup PPU.
-    LDA     #%00001000
+    INX
+    STX     $4010   
+    LDA     #%10001000
     STA     $2000
     LDA     #%00011110
     STA     $2001
@@ -74,6 +85,8 @@ RESET:
     STA     $2006
     LDX     #$00
 ;
+    JSR     CLEAR_MEM
+;
 ;   Palette loading.    
     .LOAD_PALLETE:
     LDA     TILE_PALLETE, X
@@ -82,21 +95,87 @@ RESET:
     CPX     #32
     BNE     .LOAD_PALLETE
 ;  
-;   Setup positions.  
-    LDA     #20
+;   Setup variables.  
+    LDA     #0
     STA     PCC_XPOS
     STA     PCC_YPOS
+    STA     FRAME_MOD_255
 ;    
 ;   Start looping forever.  
     JMP     FOREVER
 ;   
 ;******************************************************************************;
 ;                                                                              ;
-;   NMI: Loop forever                                                      ;
+;   NMI                                                                        ;
 ;                                                                              ;
 ;******************************************************************************;
 NMI:
-    RTI
+    LDX     <FRAME_MOD_255
+    INX     
+    STX     <FRAME_MOD_255
+    LDX     <FRAME_DELAY
+    INX
+    STX     <FRAME_DELAY
+    RTI     
+;
+;******************************************************************************;
+;                                                                              ;
+;   CLEAR_MEM                                                                  ;
+;                                                                              ;
+;******************************************************************************;
+CLEAR_MEM:
+    LDX     #$00
+    .START:
+    STA     $000,X
+    INX     
+    BNE     .START
+    RTS
+;
+;******************************************************************************;
+;                                                                              ;
+;   RENDER                                                                     ;
+;                                                                              ;
+;******************************************************************************;
+RENDER:
+;   Tell $2003 to tell $2004 to start at $0000
+    LDA     #$00
+    STA     $2003
+;   Y position.
+    LDA     <PCC_YPOS
+    STA     $2004
+;   Tile index.
+    LDA     <SPRITE_FRAME
+    STA     $2004
+;   Attributes.
+    LDA     #$00
+    STA     $2004
+;   X position.
+    LDA     <PCC_XPOS
+    STA     $2004
+;   Update.
+    LDX     <SPRITE_FRAME
+    LDY     <SPRITE_FRAME_DELAY
+    INY
+    CPY     #4
+    BNE     .END_IF1
+    LDY     #0
+    .END_IF1:
+    STY     SPRITE_FRAME_DELAY
+;    CMP     #$0
+;    BNE     .END_IF
+;    CMP     #$40
+;    BNE     .END_IF
+;    CMP     #$20
+;    BNE     .END_IF
+    CPY     #0
+    BNE     .END_IF2
+    INX
+    CPX     #2
+    BNE     .END_IF2
+    LDX     #0
+    .END_IF2:
+    STX     <SPRITE_FRAME
+    RTS
 ;
 ;******************************************************************************;
 ;                                                                              ;
@@ -104,6 +183,18 @@ NMI:
 ;                                                                              ;
 ;******************************************************************************;
 FOREVER:
+;    LDX     <FRAME_MOD_255
+;    INX
+;    STX     <FRAME_MOD_255
+;   Check if enough frame skipped.
+    .CHECK:
+    LDX     #FRAME_DELAY_AMOUNT
+    CPX     <FRAME_DELAY
+    BCS     .CHECK
+;   Otherwise, reset to 0.
+    LDX     #0
+    STX     <FRAME_DELAY
+;
     LDX     <PCC_YPOS
     INX
     STX     <PCC_YPOS
@@ -111,6 +202,7 @@ FOREVER:
     INX
     INX 
     STX     <PCC_XPOS
+    JSR     RENDER
     JSR     WAIT_VBLANK 
     JMP     FOREVER
 ;
@@ -122,18 +214,6 @@ FOREVER:
 WAIT_VBLANK:
     LDA     $2002
     BPL     WAIT_VBLANK
-;   Tell $2003 to tell $2004 to start at $0000
-    LDA     #$00
-    STA     $2003
-;   Load and store PCC_YPOS
-    LDA     <PCC_YPOS
-    STA     $2004
-    LDA     #$00
-    STA     $2004
-    LDA     #$00
-    STA     $2004
-    LDA     <PCC_XPOS
-    STA     $2004
     RTS
 ;
 ;******************************************************************************;
@@ -144,12 +224,12 @@ WAIT_VBLANK:
 TILE_PALLETE:
 ;   Backgrounds.    
     .BYTE   $0F                   ; Universal Background.
-    .BYTE   $30, $2b, $03, $0F    ; Background Palette 0
+    .BYTE   $1b, $2b, $13, $0F    ; Background Palette 0
     .BYTE   $1c, $1b, $07, $0F    ; Background Palette 1
     .BYTE   $1d, $1b, $07, $0F    ; Background Palette 2
     .BYTE   $1b, $1b, $07, $3D    ; Background Palette 3
 ;   Sprites.
-    .BYTE   $06, $03, $07, $0F    ; Sprite Palette 0
+    .BYTE   $27, $07, $30, $0F    ; Sprite Palette 0
     .BYTE   $1b, $1b, $07, $0F    ; Sprite Palette 1
     .BYTE   $1b, $1b, $07, $0F    ; Sprite Palette 2
     .BYTE   $1b, $1b, $06         ; Sprite Palette 3
@@ -194,21 +274,38 @@ BACKGROUND_DATA_END:
 ;   Sprites.                                                                   ;
 ;                                                                              ;
 ;******************************************************************************;
-;   Sprite 0.
-    .BYTE   %11111111   ; 1st plane.
-    .BYTE   %10000001   
-    .BYTE   %10000001   
-    .BYTE   %10000001
-    .BYTE   %10000001
-    .BYTE   %10000001
-    .BYTE   %10000001
-    .BYTE   %10000001   
+;   Sprite 0: Main Hero frame 0.
+    .BYTE   %00111100   ; 1st plane.
+    .BYTE   %00111100   
+    .BYTE   %00101000   
+    .BYTE   %01111110
+    .BYTE   %00111100
+    .BYTE   %00000000
+    .BYTE   %00000000
+    .BYTE   %00110000
     .BYTE   %00000000   ; 2nd plane.
+    .BYTE   %00111100   
+    .BYTE   %00111100   
     .BYTE   %00000000
     .BYTE   %00000000
+    .BYTE   %00111100
+    .BYTE   %00111100   
+    .BYTE   %00001100
+;   Sprite 0: Main Hero frame 1.
+    .BYTE   %00111100   ; 1st plane.
+    .BYTE   %00111100   
+    .BYTE   %00010100   
+    .BYTE   %01111110
+    .BYTE   %00111100
     .BYTE   %00000000
     .BYTE   %00000000
-    .BYTE   %11111111 
-    .BYTE   %00000000   
-    .BYTE   %00000001
+    .BYTE   %00001100
+    .BYTE   %00000000   ; 2nd plane.
+    .BYTE   %00111100   
+    .BYTE   %00111100   
+    .BYTE   %00000000
+    .BYTE   %00000000
+    .BYTE   %00111100
+    .BYTE   %00111100   
+    .BYTE   %00110000
 ;
